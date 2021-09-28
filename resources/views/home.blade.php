c6f61cdb6c31e5142a1dd9b99915b8086302003a
radtech-dev21 <pankaj@radtech.pro>
2021-09-28 18:54:38 +0530

    add new search page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,19 +1,20 @@
 @extends('layouts.app')
-
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-12">
             <div class="card">
                 <div class="card-header">{{ __('Dashboard') }}</div>
-
                 <div class="card-body">
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
                             {{ session('status') }}
                         </div>
                     @endif
-
+                    <div class="search">
+                        <i class="fa fa-search"></i>
+                        <input type="text" class="form-control" placeholder="Have a question? Ask Now"/>
+                    </div>
                     {{ __('You are logged in!') }}
                 </div>
             </div>
