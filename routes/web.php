785f868a6b279029ee44578a8e2d946f349cc4d8
priyankarad <priyanka@radtech.pro>
2021-09-28 16:55:07 +0530

    28-09-2021

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\RecruiterController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -24,3 +24,5 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('recruiter',[RecruiterController::class,'index']);
