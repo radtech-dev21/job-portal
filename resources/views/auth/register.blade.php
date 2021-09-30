ec610360959751198932ac21439f3ca3f116d29e
himanshu-sharma01 <himanshu@radtech.pro>
2021-09-30 18:12:36 +0530

    fix phone number validation msg

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -40,10 +40,10 @@
                             </div>
                         </div>
                         <div class="form-group row">
-                            <label for="phone" class="col-md-4 col-form-label text-md-right">{{ __('Phone Number') }}</label>
+                            <label for="phone_no" class="col-md-4 col-form-label text-md-right">{{ __('Phone Number') }}</label>
                             <div class="col-md-6">
-                            <input id="phone" type="tel" placeholder="+91 ********" class="form-control @error('phone') is-invalid @enderror" name="phone_no" value="{{ old('phone_no') }}" pattern="[0-9]{10}" required autocomplete="phone">
-                                @error('phone')
+                            <input id="phone_no" type="tel" placeholder="+91 ********" class="form-control @error('phone_no') is-invalid @enderror" name="phone_no" value="{{ old('phone_no') }}" pattern="[0-9]{10}" required autocomplete="phone">
+                                @error('phone_no')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                     </span>
