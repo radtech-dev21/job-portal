bf9be50bcf483811fdfa534e4c999a19fc151bf6
himanshu-sharma01 <himanshu@radtech.pro>
2021-09-30 18:02:16 +0530

    change mail msg

--- resources/views/mail/signup-email.blade.php
+++ resources/views/mail/signup-email.blade.php
@@ -2,7 +2,7 @@ Hello {{$email_data['name']}}
 <br><br>
 Welcome to my Website!
 <br>
-Please click the below link to verify your email and activate your account!
+Please verify your email With given OTP and activate your account!
 <h1>{{$email_data['verification_code']}}</h1>
 <br><br>
 <!-- <a href="http://127.0.0.1:8000/login/">Click Here!</a> -->
