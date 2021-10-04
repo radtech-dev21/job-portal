5823f4438a07a538bb8acc4b6a358395287ce596
himanshu-sharma01 <himanshu@radtech.pro>
2021-10-05 01:10:57 +0530

    change ui

--- resources/views/hirer-signup.blade.php
+++ resources/views/hirer-signup.blade.php
@@ -12,18 +12,51 @@
 <div class="container register">
     <div class="row">
         <div class="col-md-3 register-left">
-            <img src="https://image.ibb.co/n7oTvU/logo_white.png" alt=""/>
+            <img src="https://image.ibb.co/n7oTvU/logo_white.png" alt="" />
             <h3>Welcome</h3>
         </div>
         <div class="col-md-9 register-right">
             <div class="tab-content" id="myTabContent">
-                <h3  class="register-heading"><a href="{{ url('searchProfile') }}" class="btn btn-primary btn-lg" tabindex="-1" role="button">{{ __('Search Profile') }}</a></h3>
+                <h3 class="register-heading"><a href="{{ url('searchProfile') }}" class="btn btn-primary btn-lg" tabindex="-1" role="button">{{ __('Search Profile') }}</a></h3>
             </div>
         </div>
     </div>
+    <br>
+    <div class="col-md-12">
+        <table class="table table-bordered border-primary">
+            <thead class="table-dark">
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Pending</th>
+                    <th scope="col">Accept</th>
+                    <th scope="col">Decline</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <th scope="row">1</th>
+                    <td>Mark</td>
+                    <td>Otto</td>
+                    <td>@mdo</td>
+                </tr>
+                <tr>
+                    <th scope="row">2</th>
+                    <td>Jacob</td>
+                    <td>Thornton</td>
+                    <td>@fat</td>
+                </tr>
+                <tr>
+                    <th scope="row">3</th>
+                    <td>Larry the Bird</td>
+                    <td>Larry the Bird</td>
+                    <td>@twitter</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
 </div>
 <script>
-    $(function () {
+    $(function() {
         $('.selectpicker').selectpicker();
     });
 </script>
