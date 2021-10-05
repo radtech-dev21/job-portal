58373f0000c09ea2af561a91f2d17ae61d36ae57
priyankarad <priyanka@radtech.pro>
2021-10-05 16:10:45 +0530

    priyanka 05-10-2021

--- app/Http/Controllers/admin/HomeController.php
+++ app/Http/Controllers/admin/HomeController.php
@@ -10,6 +10,13 @@ class HomeController extends Controller
 {
 
     public function index(){
-        return view('admin/dashboard');
+    	$user = auth()->user();
+    	if(!empty($user->role) && $user->role == 'Admin'){
+    		$data = array();
+    		$data['page_name'] = 'Dashboard';
+    		return view('admin/dashboard',array('data' => $data));
+    	}else{
+    		return redirect('/admin/login');
+    	}
     }
 }
