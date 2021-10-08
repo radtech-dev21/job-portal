5a03d2a41ca3045f3f8f67cb721b3574caa4d353
radtech-dev21 <pankaj@radtech.pro>
2021-10-08 14:29:17 +0530

    conflict resolved after merge

--- routes/hirer.php
+++ routes/hirer.php
@@ -1,7 +1,7 @@
 <?php
 use App\Http\Controllers\{HirerController, ConnectionController};
 Route::group(['prefix' => 'hirer', Auth::check() => 'role:hirer'], function () {
-	Route::get('chat', [HirerController::class, 'chatView'])->name('hirer-chat')
+	Route::get('chat', [HirerController::class, 'chatView'])->name('hirer-chat');
 });
 Route::group(['prefix' => 'hirer',  'middleware' => 'auth'], function(){
 	Route::post('save',[HirerController::class,'saveHirer']);
