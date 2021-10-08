e9eec3f193cbc5873a92cc0615b6f849e8e059dd
radtech-dev21 <pankaj@radtech.pro>
2021-10-08 16:52:41 +0530

    changes in route filw

--- routes/hirer.php
+++ routes/hirer.php
@@ -1,9 +1,7 @@
 <?php
 use App\Http\Controllers\{HirerController, ConnectionController};
-Route::group(['prefix' => 'hirer', Auth::check() => 'role:hirer'], function () {
+Route::group(['prefix' => 'hirer', Auth::check() => 'role:hirer', 'middleware' => 'auth'], function () {
 	Route::get('chat', [HirerController::class, 'chatView'])->name('chat');
-});
-Route::group(['prefix' => 'hirer',  'middleware' => 'auth'], function(){
 	Route::post('save',[HirerController::class,'saveCompany'])->name('save-company');
 	Route::get('dashboard',[HirerController::class,'index'])->name('hirer-dashboard');
 	Route::get('create-company', [HirerController::class, 'createCompany'])->name('create-company');
